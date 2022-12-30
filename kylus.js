795bc617acf03288d3478514b18cb1f4f63d5540
Glitch (oil-band-grape) <none>
2022-12-30 22:35:26 +0000

    🏪⭐️ Checkpoint ./kylus.js:3676153/5377

--- kylus.js
+++ kylus.js
@@ -1,37 +1,36 @@
 var mineflayer = require("mineflayer");
 var db = require("quick.db");
-  
+
 var ayar = {
-  host: "Sunucu-ip", //Sunucu IPnizi giriniz.
-  port: 34297, //Sunucu portunuzu giriniz.
+  host: "Kylus.aternos.me", //Sunucu IPnizi giriniz.
+  port: 26487, //Sunucu portunuzu giriniz.
   username: "KylusYouTube", //Sunucuya giriş yapacak bot isminizi girin.
-  version: false //Burayı değiştirmeyin.
+  version: false, //Burayı değiştirmeyin.
 };
 
-  
 var kayit = {
   authme: "yok", //Eğer sunucunuzda AuthMe eklentisi yoksa bu var yazısını yok olarak değiştirin.
-  sifre: "ADMIN" //Buraya AuthMe varsa botun giriş yapması için şifreyi girin.
+  sifre: "ADMIN", //Buraya AuthMe varsa botun giriş yapması için şifreyi girin.
 };
-  
-var automessage = false; //5 Dakika'da bir sunucuda botun mesaj atmasını istemiyorsan true yazısını false olarak değiştir.
-  
+
+var automessage = true; //5 Dakika'da bir sunucuda botun mesaj atmasını istemiyorsan true yazısını false olarak değiştir.
+
 var bot = mineflayer.createBot(ayar);
-  
-bot.on("chat", function(username, message) {
+
+bot.on("chat", function (username, message) {
   if (username === bot.username) return;
   function intervalFunc() {
     bot.setControlState("sprint", true);
   }
   setInterval(intervalFunc, 7000);
-  
+
   if (kayit.authme == "var") {
     let giris = db.fetch(`giris_${ayar.host}_${ayar.username}`);
     if (!giris) {
       bot.chat(`/register ${kayit.sifre} ${kayit.sifre}`); //Kayıt olmasını sağladık.
       console.log("Bot kayıt oldu!");
       db.set(`giris_${ayar.host}_${ayar.username}`, "tm");
-  
+
       if (automessage == true) {
         setInterval(() => {
           bot.chat("Kylus YouTube : https://www.youtube.com/Kylus"); // değiştirmek çok basit '' arasındaki yazıyı değiştirin yeter
@@ -41,7 +40,7 @@ bot.on("chat", function(username, message) {
     if (giris) {
       bot.chat(`/login ${kayit.sifre}`); //Giriş yapmasını sağladık.
       console.log("Bot giriş yaptı!");
-  
+
       if (automessage == true) {
         setInterval(() => {
           bot.chat("Kylus YouTube : https://www.youtube.com/Kylus"); // değiştirmek çok basit '' arasındaki yazıyı değiştirin yeter
@@ -50,42 +49,40 @@ bot.on("chat", function(username, message) {
     }
   }
 });
-  
+
 bindEvents(bot);
 function bindEvents(bot) {
-  bot.on("error", function(err) {
+  bot.on("error", function (err) {
     console.log("Bir hata oluştu!");
   });
-  
-  bot.on("end", function() {
+
+  bot.on("end", function () {
     console.log("Bot sunucudan atıldı!");
     setTimeout(relog, 5000);
   });
-  
+
   function relog() {
     console.log("Sunucuya Tekrardan Baglaniliyor...");
     bot = mineflayer.createBot(ayar);
-    bot.on("chat", function(username, message) {
+    bot.on("chat", function (username, message) {
       if (username === bot.username) return;
-  
+
       console.log("Bot tekrardan oyuna giriş yaptı!");
       bot.chat(`/login ${kayit.sifre}`);
-        
-      bot.setControlState('sprint', true)
+
+      bot.setControlState("sprint", true);
     });
-  
+
     bindEvents(bot);
   }
 }
-  
-  
+
 //Yeni Kod:
-const http = require('http');
-const express = require('express');
+const http = require("http");
+const express = require("express");
 const app = express();
 app.get("/", (request, response) => {
   response.sendStatus(200);
-    
 });
 app.listen(process.env.PORT);
 setInterval(() => {
